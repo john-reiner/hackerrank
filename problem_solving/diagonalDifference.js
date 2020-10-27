8e86de1cb5ac6e624c1d2409931857878eef7419
john-reiner <johnreiner1990@gmail.com>
2020-10-27 19:36:56 -0400

    my code before submit

--- problem_solving/diagonalDifference.js
+++ problem_solving/diagonalDifference.js
@@ -1,4 +1,11 @@
 function diagonalDifference(arr) {
-    
-
+    var primary = 0
+    var secondary = 0
+    for (let i = 0; i < arr.length; i++) {
+        primary += arr[i][i]
+        secondary += arr[i][arr.length - 1 - i];
+    }
+    return Math.abs(primary - secondary)
 }
+
+console.log(diagonalDifference([ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ]))
