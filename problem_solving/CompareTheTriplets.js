44e4dba968dcb66e2886b4f2c61581e1a143ab82
john-reiner <johnreiner1990@gmail.com>
2020-10-27 16:57:21 -0400

    first commit

---
+++ problem_solving/CompareTheTriplets.js
@@ -0,0 +1,16 @@
+// Function Description
+
+// Complete the function compareTriplets in the editor below.
+
+// compareTriplets has the following parameter(s):
+
+// int a[3]: Alice's challenge rating
+// int b[3]: Bob's challenge rating
+// Return
+
+// int[2]: Alice's score is in the first position, and Bob's score is in the second.
+// Input Format
+
+// The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
+// The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
+
