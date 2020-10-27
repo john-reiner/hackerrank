7019be0e6f8f960ea58acc67f88ca22c621d8554
john-reiner <johnreiner1990@gmail.com>
2020-10-27 17:10:55 -0400

    my working solution before submiting

--- problem_solving/CompareTheTriplets.js
+++ problem_solving/CompareTheTriplets.js
@@ -14,3 +14,19 @@
 // The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
 // The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
 
+function compareTriplets(a, b) {
+    let returnArray = [0,0]
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            returnArray[0] ++
+        } 
+        if (a[i] < b[i]) {
+            returnArray[1] ++
+        } 
+        
+    }
+    return returnArray
+}
+
+console.log(compareTriplets([1,2,3] , [99,99,99]))
+console.log(compareTriplets([100,2,3] , [99,99,99]))
