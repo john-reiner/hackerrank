a329002d87409d0c983fbbde89726807ec9910fc
john-reiner <johnreiner1990@gmail.com>
2020-10-27 21:40:30 -0400

    before tests

--- problem_solving/birthdayCakeCandles.js
+++ problem_solving/birthdayCakeCandles.js
@@ -1,4 +1,10 @@
 function birthdayCakeCandles(candles) {
-    // Write your code here
-
+    let maxValue = Math.max(...candles)
+    let count = 0
+    for (let i = 0; i < candles.length; i++) {
+        candles[i] === maxValue && count ++
+    }
+    return count 
 }
+
+console.log(birthdayCakeCandles([3,2,1,3]))
