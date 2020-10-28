2955c1df55e67396c577c21f415568f77c5b5b1d
john-reiner <johnreiner1990@gmail.com>
2020-10-27 20:05:05 -0400

    passed test case

--- problem_solving/plusMinus.js
+++ problem_solving/plusMinus.js
@@ -1,4 +1,17 @@
 function plusMinus(arr) {
-
-
+    let returnedArray = [0,0,0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            returnedArray[0] ++
+        } else if (arr[i] < 0) {
+            returnedArray[1] ++
+        } else {
+            returnedArray[2] ++
+        }
+    }
+    console.log((returnedArray[0] / arr.length).toFixed(6))
+    console.log((returnedArray[1] / arr.length).toFixed(6))
+    console.log((returnedArray[2] / arr.length).toFixed(6))
 }
+
+console.log(plusMinus([1,1,0,-1,-1]))
