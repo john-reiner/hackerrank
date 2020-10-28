7a1d5e5d10879c838abd2257697b64842697e6b9
john-reiner <johnreiner1990@gmail.com>
2020-10-28 05:01:40 -0400

    before submit

--- problem_solving/simpleArraySum.js
+++ problem_solving/simpleArraySum.js
@@ -1,3 +1,9 @@
 function simpleArraySum(ar) {
-    
+    let counter = 0
+    for (let i = 0; i < ar.length; i++) {
+        counter += ar[i]
+    }
+    return counter
 }
+
+console.log(simpleArraySum([1,2,3,4,5]))
