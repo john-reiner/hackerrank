58841ae7ca5e2423f59160ffee0073590a949dad
john-reiner <johnreiner1990@gmail.com>
2020-10-30 09:31:50 -0400

    code before tests

--- problem_solving/kangaroo.js
+++ problem_solving/kangaroo.js
@@ -1,4 +1,25 @@
 function kangaroo(x1, v1, x2, v2) {
+    let k1JumpCount = 0
+    let k1Pos = x1
+    let k2Pos = x2 
+    let running = true
+    while (running) {
+        k1JumpCount ++
+        k2JumpCount ++
+        k1Pos += v1
+        k2Pos += v2
+        if (k1Pos === k2Pos) {
+            return "YES"
+        } else {
 
-
+        }
+        if (k1JumpCount >= 10000) {
+            running = false
+            return "NO"
+        }
+    }
 }
+
+
+//
+console.log(kangaroo(0,3,4,2))
