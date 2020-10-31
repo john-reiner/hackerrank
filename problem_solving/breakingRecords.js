41fce2a3fe48cd3f91a4ca27335c8365c622b2ec
john-reiner <johnreiner1990@gmail.com>
2020-10-31 10:28:30 -0400

    setting up breakingRecords

---
+++ problem_solving/breakingRecords.js
@@ -0,0 +1,7 @@
+function breakingRecords(scores) {
+    for (let i = 0; i < scores.length; i++) {
+        console.log(scores[i])
+    }
+}
+
+console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]))
