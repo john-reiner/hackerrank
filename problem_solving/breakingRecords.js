25ec39c8f008d87412aea0a98c62a194ff0206bb
john-reiner <johnreiner1990@gmail.com>
2020-10-31 10:39:11 -0400

    passed all tests

--- problem_solving/breakingRecords.js
+++ problem_solving/breakingRecords.js
@@ -1,7 +1,16 @@
 function breakingRecords(scores) {
+    let highLow = [0,0]
+    let high = scores[0]
+    let low = scores[0]
     for (let i = 0; i < scores.length; i++) {
         console.log(scores[i])
+        if (scores[i] > high) {
+            high = scores[i]
+            highLow[0] ++
+        } else if (scores[i] < low) {
+            low = scores[i]
+            highLow[1] ++
+        }
     }
+    return highLow
 }
-
-console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]))
