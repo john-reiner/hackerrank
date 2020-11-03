45adf02b7ff2a4f8a4848c8e3e053df09211ef68
john-reiner <johnreiner1990@gmail.com>
2020-11-02 19:22:24 -0500

    passed all tests

--- problem_solving/divisibleSumPairs.js
+++ problem_solving/divisibleSumPairs.js
@@ -1,6 +1,12 @@
 function divisibleSumPairs(n, k, ar) {
-
-
+    let count = 0
+    for (let i = 0; i < n; i++) {
+        for (let j = (i + 1); j < n; j++) {
+            if ((ar[i] + ar[j]) % k === 0 ) count++
+        }
+    }
+    return count
 }
 
-console.log(divisibleSumPairs(6, 3, [ 1, 3, 2, 6, 1, 2 ]))
+console.log(divisibleSumPairs(6, 5, [ 1, 2, 3, 4, 5, 6 ]))
+
