e4dd2bf91fff11a90ce08f9b2ab5ce4508ec6fa7
john-reiner <johnreiner1990@gmail.com>
2020-11-02 20:23:42 -0500

    passed all tests

--- problem_solving/migratoryBirds.js
+++ problem_solving/migratoryBirds.js
@@ -1,5 +1,9 @@
 function migratoryBirds(arr) {
-
+    let types = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    for (let i = 0; i < arr.length; i++) types[arr[i]] ++
+    let typesArray = Object.values(types) 
+    let maxValue = Math.max(...typesArray)
+    return typesArray.indexOf(maxValue) + 1
 }
 
 console.log(migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]))
